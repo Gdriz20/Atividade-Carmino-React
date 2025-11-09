7ebba54a1cea7a49c3169df0157539fbc1296809
Kleber Tadeu <klebertadeu17@gmail.com>
2025-11-09 01:15:46 -0300

    Instalação da lib Routers
    Novos arquivos precisam ser instalados para que as rotas funcionem.

--- blog-react/src/components/main.tsx
+++ blog-react/src/components/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Header.css'
 import './footer.css'
-import Pesquisa from './pesquisa/pesquisa.tsx'
+import Cadastro from './cadastro/Cadastro.tsx'
 
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Pesquisa />
+    <Cadastro />
   </StrictMode>,
 )
