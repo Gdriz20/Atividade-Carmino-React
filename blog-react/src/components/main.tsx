6c7282072cf3b75c4f859bf949d234c8777917ad
Kleber Tadeu <klebertadeu17@gmail.com>
2025-11-09 00:21:53 -0300

    Tela de pesquisa
    A tela "importada" da vez, foi a de pesquisa, onde foi implementado um a função de contagem de resultados, porém, ainda feita no próprio código, apenas para simulação

--- blog-react/src/components/main.tsx
+++ blog-react/src/components/main.tsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Header.css'
 import './footer.css'
-import Cadastro from './cadastro/Cadastro.tsx'
-import AssNews from './newsletter/AssNews.tsx'
+import Pesquisa from './pesquisa/pesquisa.tsx'
 
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AssNews />
+    <Pesquisa />
   </StrictMode>,
 )
