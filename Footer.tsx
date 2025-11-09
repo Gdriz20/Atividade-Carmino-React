b3451b7259e88b8a1c34d57e6277a234dc997106
Gabriel Martins <gabrielmoraesmartins812@gmail.com>
2025-11-08 21:25:17 -0300

    Footer.tsx
    O arquivo Footer.tsx é um componente React responsável por renderizar o rodapé do seu site ou aplicativo. O footer serve para serve para conter informações que aparecem no final de todas as páginas, como:
    Créditos ou direitos autorais
    Links de redes sociais
    Contatos e do mesmo jeito, a sua utilidade foi usada agora.

---
+++ Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="tags-footer">
+        <span>Instagram</span>
+        <span>Work</span>
+        <span>Bags</span>
+        <span>Lamp</span>
+        <span>Books</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
