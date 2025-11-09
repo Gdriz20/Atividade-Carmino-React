5e78c746bdd46acc9fd4f473858b3a3908c41919
Kleber Tadeu <klebertadeu17@gmail.com>
2025-11-09 14:13:48 -0300

    Configuração inicial das rotas
    Main e App configurados para receberem os demais componentes como rotas. Componentes header e footer são fixos em App, pois estão em todas as páginas, por enquanto. Único elemento que o react precisa renderizar é o corpo da página.

--- blog-react/src/components/App.tsx
+++ blog-react/src/components/App.tsx
@@ -1,6 +1,8 @@
 import Cadastro from "./cadastro/Cadastro";
 import Footer from "./footer/footer";
 import Header from "./header/header";
+import { Outlet } from "react-router-dom";
+
 
 function App(){
 
@@ -8,7 +10,7 @@ function App(){
         <>
         
         <Header />
-            <Cadastro />
+            <Outlet />
         <Footer />
         
         </>
--- blog-react/src/components/main.tsx
+++ blog-react/src/components/main.tsx
@@ -1,12 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './login/Login.tsx'
+import Cadastro from './cadastro/Cadastro.tsx'
 
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Login />
+      },
+      {
+        path: "/cadastro",
+        element: <Cadastro />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
